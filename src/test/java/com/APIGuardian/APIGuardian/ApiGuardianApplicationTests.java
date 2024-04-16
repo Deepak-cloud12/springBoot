e8027236fa93812fc73f6e9e60dae713ae0d058f
Deepak <b170179@nitsikkim.ac.in>
2024-04-16 17:29:48 +0530

    remotr repo

---
+++ src/test/java/com/APIGuardian/APIGuardian/ApiGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APIGuardian.APIGuardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
